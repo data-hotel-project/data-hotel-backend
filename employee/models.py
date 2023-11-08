533920d66cc29641133871990bbe895d9396beed
RafaelFreitas-cloud <raf.mec.ba@gmail.com>
2023-11-08 09:10:49 -0300

    feat/continious models

---
+++ employee/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class FunctionChoice(models.TextChoices):
+    ADMIN = "Admin"
+    RECEPTIONIST = "Receptionist"
+    REGULAR = "Regular"
+
+
+class Employee(AbstractUser):
+    birthdate = models.DateTimeField()
+    nationality = models.CharField(max_length=20)
+    contact = models.CharField(max_length=11)
+    contact_aditional = models.CharField(max_length=11, null=True)
+    emergency_num = models.CharField(max_length=11)
+    job_function = models.CharField(max_length=20, choices=FunctionChoice.choices, default=FunctionChoice.REGULAR)
+    admission_date = models.DateTimeField()
+    is_working = models.BooleanField(default=True)
+
+    address = models.OneToOneField("address.Address", on_delete=models.DO_NOTHING)
+
+    hotel = models.OneToOneField("hotel.Hotel", on_delete=models.CASCADE)
+
