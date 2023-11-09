6490101ad3e0cd197295e321d71ad79beb4a0fc5
Matheus M <matheus150101miranda@gmail.com>
2023-11-08 21:43:57 -0300

    feat: employee view in progress

--- employee/urls.py
+++ employee/urls.py
@@ -1,8 +1,9 @@
+from . import views
 from django.urls import path
-from employee import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view()),
-    path('refresh/', TokenRefreshView.as_view()),
+    path("employee/", views.EmployeeView.as_view()),
+    path("login/", TokenObtainPairView.as_view()),
+    path("refresh/", TokenRefreshView.as_view()),
 ]
