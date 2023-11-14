b24a01fd2c686f0c55474b1a3182646dc5887228
RafaelFreitas-cloud <raf.mec.ba@gmail.com>
2023-11-14 12:15:14 -0300

    refactor/try to implement autentication on reservation

--- reservation/serializer.py
+++ reservation/serializer.py
@@ -6,3 +6,4 @@ class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
         fields = "__all__"
+        read_only_fields = ["guest"]
--- reservation/views.py
+++ reservation/views.py
@@ -1,14 +1,34 @@
 from rest_framework import generics
 from .serializer import ReservationSerializer
 from .models import Reservation
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class ReservationListCreateView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    # mudar para "ser GUEST"
+    # permission_classes = [IsAuthenticated]
+
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
 
+    def post(self, request, *args, **kwargs):
+        from ipdb import set_trace
+        set_trace()
+        return super().post(request, *args, **kwargs)
+
+
+    def perform_create(self, serializer):
+        print("eu cheiguei aqui")
+        return serializer.save(guest=self.request.user.id)
+
 
 class ReservationRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    # mudar para "ser GUEST DONO"
+    permission_classes = [IsAuthenticated]
+    
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
     lookup_url_kwarg = "pk"
