ca526cd13b3268919ba4cd35b89fc51eb3806d4d
Matheus M <matheus150101miranda@gmail.com>
2023-11-17 09:17:26 -0300

    refactor: add requirements.txt

--- employee/models.py
+++ employee/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from address.models import Address
-from guest.models import CustomUser
+from customUser.models import CustomUser
 from hotel.models import Hotel
 
 
--- guest/models.py
+++ guest/models.py
@@ -1,22 +1,7 @@
-import uuid
-
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from address.models import Address
-
-
-class CustomUser(AbstractUser):
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    birthdate = models.DateField(null=True)
-    nationality = models.CharField(max_length=20)
-    contact = models.CharField(max_length=50)
-    contact_aditional = models.CharField(max_length=11, blank=True, default="")
-    emergency_num = models.CharField(max_length=11)
-    email = models.EmailField(max_length=50, unique=True, blank=True)
-
-    # class Meta:
-    #     abstract = True
+from customUser.models import CustomUser
 
 
 class Guest(CustomUser):
