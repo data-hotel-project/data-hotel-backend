64a849fa14940e61a2c68d3f3869acab221dab41
Matheus M <matheus150101miranda@gmail.com>
2023-11-21 14:20:56 -0300

    workspace updated

---
+++ utils/fields/customUser_fields.py
@@ -0,0 +1,21 @@
+class CustomUserFields:
+    fields = [
+        "id",
+        "username",
+        "email",
+        "password",
+        "birthdate",
+        "nationality",
+        "contact",
+        "contact_aditional",
+        "emergency_num",
+        "job_function",
+        "admission_date",
+        "is_working",
+        "is_staff",
+        "is_superuser",
+    ]
+    read_only_fields = ["id", "is_superuser"]
+    extra_kwargs = {
+        "password": {"write_only": True, "required": True},
+    }
