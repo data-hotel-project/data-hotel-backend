7674fbef049b31ea32779a906e48bbf4bda44cce
RafaelFreitas-cloud <raf.mec.ba@gmail.com>
2023-11-21 08:56:37 -0300

    feat/getLogged route

--- customUser/views.py
+++ customUser/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializer import CustomUserSerializer
+from .models import CustomUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from utils.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+class GetLoggedUser(generics.RetrieveAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = CustomUserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+    def get(self, request, *args, **kwargs):
+        user = self.get_object()
+        serializer = self.get_serializer(user)
+        return Response(serializer.data)
+
