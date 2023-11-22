fdec0c4fc4f853728696f444d08bb88fd0e19569
Matheus M <matheus150101miranda@gmail.com>
2023-11-22 12:42:36 -0300

    fix: adjustment in back

--- address/views.py
+++ address/views.py
@@ -1,13 +1,23 @@
 from rest_framework import generics
+from rest_framework.response import Response
 from .serializer import AddressSerializer
 from .models import Address
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .permissions import IsAddressOwner
 from utils.permissions import IsAdmin
 
+
 class AddressListCreateView(generics.ListCreateAPIView):
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        queryset = queryset.exclude(street__endswith="admin")
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class AddressRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
--- hotel/views.py
+++ hotel/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.response import Response
 
 from .models import Hotel
 from .serializer import HotelSerializer
@@ -7,6 +8,14 @@ from .serializer import HotelSerializer
 class HotelListCreateView(generics.ListCreateAPIView):
     queryset = Hotel.objects.all()
     serializer_class = HotelSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        queryset = queryset.exclude(name__endswith="admin")
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class HotelRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
